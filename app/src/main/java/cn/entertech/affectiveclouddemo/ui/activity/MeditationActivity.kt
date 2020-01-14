142c2f25ebcd979589bed6971c17c5c490cbb23d
lloyd <651519531@qq.com>
2020-01-14 15:00:18 +0800

    体验页适配平板

--- app/src/main/java/cn/entertech/affectiveclouddemo/ui/activity/MeditationActivity.kt
+++ app/src/main/java/cn/entertech/affectiveclouddemo/ui/activity/MeditationActivity.kt
@@ -1,19 +1,35 @@
 package cn.entertech.affectiveclouddemo.ui.activity
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import cn.entertech.affectiveclouddemo.R
 import cn.entertech.affectiveclouddemo.ui.fragment.MeditationFragment
-import kotlinx.android.synthetic.main.activity_meditation.*
+import cn.entertech.affectiveclouddemo.ui.fragment.MeditationLargeFragment
 
 class MeditationActivity : BaseActivity() {
+
+    private var fragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meditation)
+        initFragment()
     }
 
+    fun initFragment() {
+        if (resources.displayMetrics.widthPixels > resources.displayMetrics.heightPixels) {
+            fragment = MeditationLargeFragment()
+        } else {
+            fragment = MeditationFragment()
+        }
+        supportFragmentManager.beginTransaction().replace(R.id.fl_container, fragment!!).commitAllowingStateLoss()
+    }
 
     override fun onBackPressed() {
-        (fragment_meditaiton as MeditationFragment).showDialog()
+        if (fragment is MeditationFragment) {
+            (fragment as MeditationFragment).showDialog()
+        } else {
+            (fragment as MeditationLargeFragment).showDialog()
+        }
     }
 }
