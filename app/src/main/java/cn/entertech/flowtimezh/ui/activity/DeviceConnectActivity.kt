b6887a49e4eebd0ddf4bbf8e7f0153171169798f
lloyd0577 <651519531@qq.com>
2022-11-15 17:38:18 +0800

    设备连接UI

---
+++ app/src/main/java/cn/entertech/flowtimezh/ui/activity/DeviceConnectActivity.kt
@@ -0,0 +1,12 @@
+package cn.entertech.flowtimezh.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cn.entertech.flowtimezh.R
+
+class DeviceConnectActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_device_connect)
+    }
+}
