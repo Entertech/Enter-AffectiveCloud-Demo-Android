30b6f268b8b57fa948a57aa2d87a25c7c9b4ca8c
lloyd0577 <651519531@qq.com>
2022-11-21 11:18:23 +0800

    报表滑动优化

--- app/src/main/java/cn/entertech/flowtimezh/ui/fragment/StatisticsDataFragment.kt
+++ app/src/main/java/cn/entertech/flowtimezh/ui/fragment/StatisticsDataFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
-import androidx.core.content.ContextCompat
 import androidx.core.widget.NestedScrollView
 import androidx.recyclerview.widget.LinearLayoutManager
 import cn.entertech.flowtimezh.R
@@ -19,22 +18,15 @@ import cn.entertech.flowtimezh.database.MeditationDao
 import cn.entertech.flowtimezh.database.MeditationLabelsDao
 import cn.entertech.flowtimezh.database.UserLessonRecordDao
 import cn.entertech.flowtimezh.database.model.MeditationLabelsModel
-import cn.entertech.flowtimezh.ui.activity.MeditationActivity
 import cn.entertech.flowtimezh.ui.activity.MeditationDimListActivity
 import cn.entertech.flowtimezh.ui.activity.MeditationLabelsCommitActivity
-import cn.entertech.flowtimezh.ui.activity.MeditationTimeRecordActivity
 import cn.entertech.flowtimezh.ui.adapter.MeditationLabelsListAdapter
 import cn.entertech.flowtimezh.utils.TimeUtils
 import cn.entertech.flowtimezh.utils.TimeUtils.*
 import cn.entertech.flowtimezh.utils.reportfileutils.FileHelper
 import cn.entertech.flowtimezh.utils.reportfileutils.MeditationReportDataAnalyzed
-import cn.entertech.uicomponentsdk.report.StackedAreaChart
-import cn.entertech.uicomponentsdk.utils.removeZeroData
 import com.chad.library.adapter.base.BaseQuickAdapter
-import kotlinx.android.synthetic.main.activity_meditation_labels_commit.*
-
 import kotlinx.android.synthetic.main.fragment_statistics_data.*
-import kotlin.collections.ArrayList
 
 class StatisticsDataFragment : androidx.fragment.app.Fragment() {
     private var meditaitonId: Long? = null
